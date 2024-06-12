74f4eb4119ffd486fc32b96c35f76c4981546588
Andrew Arderne <andrew.arderne@ravelin.com>
2024-06-12 08:46:12 +0100

    add netcat server and refactor

---
+++ servers/go-server.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net"
+)
+
+func handleConnection(conn net.Conn) {
+	defer conn.Close()
+
+	buffer := make([]byte, 4096)
+	// This is where the server should hang, the slowloris
+	// should continually spin this loop.
+	for {
+		n, err := conn.Read(buffer)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading:", err)
+			return
+		}
+		request := string(buffer[:n])
+		fmt.Println("Data sent:", request)
+
+	}
+
+	response := "HTTP/1.1 200 OK\r\n" +
+		"Content-Type: text/plain\r\n" +
+		"Content-Length: 13\r\n" +
+		"Connection: close\r\n\r\n" +
+		// I got confused here we ended the headers with \r\n\r\n
+		// and proceeded to write the response body, but obviously
+		// GET responses needed to contain a body.
+		"Hello, world!"
+
+	_, err := conn.Write([]byte(response))
+	if err != nil {
+		fmt.Println("Error writing response:", err)
+		return
+	}
+}
+
+func main() {
+	port := "8080"
+	listener, err := net.Listen("tcp", ":"+port)
+	if err != nil {
+		fmt.Println("Error starting server:", err)
+		return
+	}
+	defer listener.Close()
+
+	fmt.Println("Server is listening on port", port)
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting connection:", err)
+			continue
+		}
+
+		go handleConnection(conn)
+	}
+}
